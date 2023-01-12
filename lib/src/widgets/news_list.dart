c8dc1d944f70100fbc5946b4929ff233f746ea4f
IDaMariusI <fcovillanueva1988@gmail.com>
2023-01-12 10:18:51 -0600

    295. Diseno del widget de las noticias

--- lib/src/widgets/news_list.dart
+++ lib/src/widgets/news_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:newsapp/src/models/models.dart';
+import 'package:newsapp/src/theme/tema.dart';
 
 class NewsList extends StatelessWidget {
   const NewsList(this.news, {super.key});
@@ -12,8 +13,80 @@ class NewsList extends StatelessWidget {
     return ListView.builder(
       itemCount: news.length,
       itemBuilder: (BuildContext context, int index) {
-        return Text(news[index].title ?? '');
+        return _New(noticia: news[index], index: index);
       },
+    );
+  }
+}
+
+class _New extends StatelessWidget {
+  const _New({required this.noticia, required this.index});
+
+  final Article noticia;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        _TarjetaTopBar(noticia, index),
+        _TarjetaTitulo(noticia),
+        _TarjetaImagen(noticia),
+      ],
+    );
+  }
+}
+
+class _TarjetaTopBar extends StatelessWidget {
+  const _TarjetaTopBar(this.noticia, this.index);
+
+  final Article noticia;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      margin: const EdgeInsets.only(bottom: 10),
+      child: Row(
+        children: <Widget>[
+          Text(
+            '${index + 1}. ',
+            style: TextStyle(color: myTheme.colorScheme.secondary),
+          ),
+          Text('${noticia.source?.name}. '),
+        ],
+      ),
+    );
+  }
+}
+
+class _TarjetaTitulo extends StatelessWidget {
+  const _TarjetaTitulo(this.noticia);
+
+  final Article noticia;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Text(
+        '${noticia.title}',
+        style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+      ),
+    );
+  }
+}
+
+class _TarjetaImagen extends StatelessWidget {
+  const _TarjetaImagen(this.noticia);
+
+  final Article noticia;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text('Hola Mundo'),
     );
   }
 }
