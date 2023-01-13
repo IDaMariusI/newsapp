dc99bffc78dd66ea13e26ca8a4ab885792c0e80f
Martin Villanueva <fcovillanueva1988@gmail.com>
2023-01-13 11:33:23 -0600

    300. Puliendo el menú de categorias

--- lib/src/pages/tab2_page.dart
+++ lib/src/pages/tab2_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:newsapp/src/services/news_service.dart';
+
 import 'package:provider/provider.dart';
+
+import 'package:newsapp/src/models/models.dart';
+import 'package:newsapp/src/services/services.dart';
 
 class Tab2Page extends StatelessWidget {
   const Tab2Page({super.key});
@@ -29,17 +32,42 @@ class _CategoriesList extends StatelessWidget {
       scrollDirection: Axis.horizontal,
       itemCount: categories.length,
       itemBuilder: (BuildContext context, int index) {
+        final cName = categories[index].name;
+
         return Padding(
           padding: const EdgeInsets.all(8),
           child: Column(
             children: <Widget>[
-              Icon(categories[index].icon),
+              _CategoryButton(categories[index]),
               const SizedBox(height: 5),
-              Text(categories[index].name),
+              Text('${cName[0].toUpperCase()}${cName.substring(1)}'),
             ],
           ),
         );
       },
+    );
+  }
+}
+
+class _CategoryButton extends StatelessWidget {
+  const _CategoryButton(this.category);
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        width: 40,
+        height: 40,
+        margin: const EdgeInsets.symmetric(horizontal: 10),
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white,
+        ),
+        child: Icon(category.icon, color: Colors.black54),
+      ),
     );
   }
 }
